6beeadac091dafd365f25bbf3a1fd2006c0dc23b
Rahul Tech Support <94098735+HyperScream@users.noreply.github.com>
2023-02-06 20:32:30 -0500

    Update extrafun.h

--- extrafun.h
+++ extrafun.h
@@ -1,5 +1,9 @@
 #pragma once
 #include <cstdlib>
+#include <thread>
+#include <chrono>
+#include <string>
+
 string checkWin(string checkCurrentDisplayWin)
 {
 	string winCondition;
@@ -115,16 +119,21 @@ string playerInput(string checkCurrentString)
 			if (userPlayInput == 9)
 			{
 				cout << "The player has called a draw!";
-				exit( 2 );
+				std::this_thread::sleep_for(std::chrono::seconds(1));
+				exit(2);
 			}
 
 			if (userPlayInput > 8 || userPlayInput < 0)
 			{
 				cout << "Sorry, thats not a valid input" << endl;
+				cin.clear();
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
 			}
 			else if (!cin)
 			{
 				cout << "Sorry, thats not a valid input" << endl;
+				cin.clear();
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
 			}
 			else
 			{
@@ -141,9 +150,148 @@ string playerInput(string checkCurrentString)
 		else
 		{
 			cout << "Sorry, that position seems to be unavailable!" << endl;
-			//calls invalid string place
 		}
 
 	}
 	return checkCurrentString;
 }
+
+string winPotent(string anotherBoardDataType) {
+	string saveBoard = anotherBoardDataType;
+	string board = anotherBoardDataType;
+	char bot = 'x';
+	char player = 'o';
+	int win_positions[8][3] = { {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6} };
+
+	for (int i = 0; i < 8; i++) {
+		int a = win_positions[i][0];
+		int b = win_positions[i][1];
+		int c = win_positions[i][2];
+		if (board[a] == bot && board[b] == bot && board[c] == '-') {
+			board[c] = bot;
+			return board;
+		}
+		if (board[a] == bot && board[c] == bot && board[b] == '-') {
+			board[b] = bot;
+			return board;
+		}
+		if (board[b] == bot && board[c] == bot && board[a] == '-') {
+			board[a] = bot;
+			return board;
+		}
+	}
+
+	for (int i = 0; i < 8; i++) {
+		int a = win_positions[i][0];
+		int b = win_positions[i][1];
+		int c = win_positions[i][2];
+		if (board[a] == player && board[b] == player && board[c] == '-') {
+			board[c] = bot;
+			return board;
+		}
+		if (board[a] == player && board[c] == player && board[b] == '-') {
+			board[b] = bot;
+			return board;
+		}
+		if (board[b] == player && board[c] == player && board[a] == '-') {
+			board[a] = bot;
+			return board;
+		}
+	}
+
+	if (saveBoard == board) {
+		return saveBoard;
+	}
+	else {
+		return board;
+	}
+}
+
+string openingMove(string newBoardPos)
+{
+	string oldBoardPos;
+	oldBoardPos = newBoardPos;
+
+
+	char player = 'o';
+	char bot = 'x';
+
+	if (newBoardPos == "o--------" || newBoardPos == "--o------" || newBoardPos == "------o--" || newBoardPos == "--------o")
+	{
+		newBoardPos[4] = bot;
+	}
+	else if (newBoardPos == "----o----")
+	{
+		newBoardPos[8] = bot;
+	}
+	else if (newBoardPos == "-o-------")
+	{
+		newBoardPos[0] = bot;
+	}
+	else if (newBoardPos == "-------o-")
+	{
+		newBoardPos[8] = bot;
+	}
+	else if (newBoardPos == "-----o---")
+	{
+		newBoardPos[2] = bot;
+	}
+	else if (newBoardPos == "---o-----")
+	{
+		newBoardPos[6] = bot;
+	}
+	else if (newBoardPos[1] == player && newBoardPos[6] == player || newBoardPos[8] == player && newBoardPos[0] == bot)
+	{
+		if (newBoardPos[4] = '-')
+		{
+			newBoardPos[4] = bot;
+		}
+	}
+	else if (newBoardPos[3] == player && newBoardPos[8] == player || newBoardPos[2] == player && newBoardPos[6] == bot)
+	{
+		if (newBoardPos[4] = '-')
+		{
+			newBoardPos[4] = bot;
+		}
+	}
+	else if (newBoardPos[7] == player && newBoardPos[0] == player || newBoardPos[2] == player && newBoardPos[8] == bot)
+	{
+		if (newBoardPos[4] = '-')
+		{
+			newBoardPos[4] = bot;
+		}
+	}
+	else if (newBoardPos[5] == player && newBoardPos[0] == player || newBoardPos[6] == player && newBoardPos[2] == bot)
+	{
+		if (newBoardPos[4] = '-')
+		{
+			newBoardPos[4] = bot;
+		}
+	}
+	else
+	{
+		return oldBoardPos;
+	}
+	return newBoardPos;
+}
+
+string diagonalBlock(string diagonalBoardPos)
+{
+	string oldBoard;
+	oldBoard = diagonalBoardPos;
+
+	char player = 'o';
+	char bot = 'x';
+
+	if (diagonalBoardPos == "o---x---o" || diagonalBoardPos == "--o-x-o--")
+	{
+		diagonalBoardPos[3] = bot;
+	}
+	else
+	{
+		return oldBoard;
+	}
+	return diagonalBoardPos;
+
+}
+
