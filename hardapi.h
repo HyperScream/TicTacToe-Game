eb279484cc80302a5536cfe5a3b126595e2cf685
Rahul Tech Support <94098735+HyperScream@users.noreply.github.com>
2023-02-06 20:32:57 -0500

    Update hardapi.h

--- hardapi.h
+++ hardapi.h
@@ -1,3 +1,131 @@
 #pragma once
-#include <iostream>
+#include "extrafun.h"
+#include "display.h"
 
+#pragma once
+#include <random>
+#include "display.h"
+#include "extrafun.h"
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+void hardApi()
+{
+
+	string callDisplayValue{ "---------" };
+	callDisplay(callDisplayValue);
+	while (true)
+	{
+		callDisplayValue = playerInput(callDisplayValue);
+		string replaceCharVal;
+		string checkIfChange;
+		string checkIfChange2;
+
+		while (true)
+		{
+
+			checkIfChange2 = openingMove(callDisplayValue);
+			if (checkIfChange2 != callDisplayValue)
+			{
+				callDisplayValue = checkIfChange2;
+				callDisplay(callDisplayValue);
+				break;
+			}
+			else
+			{
+
+			}
+			checkIfChange = winPotent(callDisplayValue);
+			if (checkIfChange != callDisplayValue)
+			{
+				callDisplayValue = checkIfChange;
+				callDisplay(callDisplayValue);
+				break;
+			}
+			else
+			{
+
+			}
+			random_device rd;
+			int getRandomNumber = rd() % 9;
+
+			if (getRandomNumber >= callDisplayValue.length())
+			{
+				continue;
+			}
+			else
+			{
+				char replaceCharVal = callDisplayValue.at(getRandomNumber);
+
+				if (replaceCharVal != 'x' && replaceCharVal != 'o')
+				{
+					callDisplayValue.replace(getRandomNumber, 1, "x");
+					callDisplay(callDisplayValue);
+					break;
+				}
+				else
+				{
+					string z{ callDisplayValue };
+					string winBool2;
+
+					winBool2 = checkWin(z);
+					if (winBool2 == "playerWin")
+					{
+						cout << "The player has won the game!" << endl;
+						std::this_thread::sleep_for(chrono::milliseconds(5000));
+						exit(2);
+					}
+					else if (winBool2 == "playerLose")
+					{
+						cout << "The player has lost the game!" << endl;
+						std::this_thread::sleep_for(chrono::milliseconds(5000));
+						exit(2);
+					}
+					else if (winBool2 == "neither")
+					{
+						std::this_thread::sleep_for(chrono::milliseconds(10));
+					}
+					else if (winBool2 == "draw")
+					{
+						cout << "Draw!" << endl;
+						std::this_thread::sleep_for(chrono::milliseconds(5000));
+						exit(2);
+					}
+					else
+					{
+						cout << "Exception Handled: Invalid winCondition" << endl;
+					}
+				}
+			}
+
+		}
+
+		string x{ callDisplayValue };
+		string winBool;
+
+		winBool = checkWin(x);
+		if (winBool == "playerWin")
+		{
+			cout << "The player has won the game!" << endl;
+			std::this_thread::sleep_for(chrono::milliseconds(5000));
+			break;
+		}
+		else if (winBool == "playerLose")
+		{
+			cout << "The player has lost the game!" << endl;
+			std::this_thread::sleep_for(chrono::milliseconds(5000));
+			break;
+		}
+		else if (winBool == "neither")
+		{
+			std::this_thread::sleep_for(chrono::milliseconds(50));
+		}
+		else
+		{
+			cout << "Exception Handled: Invalid winCondition" << endl;
+		}
+
+	}
+}
